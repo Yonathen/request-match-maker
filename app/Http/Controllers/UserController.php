47ed2b5dc74e6c89565ea51e458783598f2ef44e
Yonathen Mekbib <yonstudy18@gmail.com>
2020-10-04 15:13:08 +0200

    check for logo index exists

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -220,7 +220,7 @@ class UserController extends Controller
             }
 
             $location = FileLocations::UPLOADS . '/'  . $retrievedUser->id . '/' . FileLocations::PROFILE;
-            if ( !is_null( $input["logo"] ) ) {
+            if ( array_key_exists("logo", $input) ) {
                 $fileUpload = $this->fileRepository->fileUploadCroppedImage($input["logo"], $location, 'profile_logo_' . $retrievedUser.id, 'png');
                 if ( $fileUpload['status'] ) {
                     $retrievedUser->logo = $fileUpload["content"][0];

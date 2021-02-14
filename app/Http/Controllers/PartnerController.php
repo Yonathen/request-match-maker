4352c8c2aad9ebf62eabd38254631f830e09722a
Yonathen Mekbib Hailemariam <Yonathan.Hailemariam@nttdata.com>
2021-02-14 13:51:04 +0100

    Testing confirm partner

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -101,7 +101,7 @@ class PartnerController extends Controller
             }
 
             $partnerToConfrim->status = PartnerStatus::CONFIRMED;
-            if ( !$this->partnerRepository->savePartner( $partnerToConfrim ) ) {
+            /*if ( !$this->partnerRepository->savePartner( $partnerToConfrim ) ) {
                 throw (new Exception("Failed to confirm partnership.", 1));
             }
 
@@ -109,7 +109,7 @@ class PartnerController extends Controller
             $this->notificationRepository->sendNotification($requestingUser, $notification);
 
 
-            Mail::to($requestingUser->email)->send(new PartnerEmail($requestingUser, $confirmingUser, NotificationType::NEW_PARTNER));
+            Mail::to($requestingUser->email)->send(new PartnerEmail($requestingUser, $confirmingUser, NotificationType::NEW_PARTNER));*/
 
             $this->returnValue = $partnerToConfrim;
         } catch (Exception $e) {
